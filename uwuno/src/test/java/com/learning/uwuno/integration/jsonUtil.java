5cfd0b728afe10f6dd873b4f034763984b097e00
Angela Chang <ayhchang11@gmail.com>
2020-09-06 02:20:23 -0700

    wip basic tests

--- uwuno/src/test/java/com/learning/uwuno/integration/jsonUtil.java
+++ uwuno/src/test/java/com/learning/uwuno/integration/jsonUtil.java
@@ -6,13 +6,17 @@ import org.junit.jupiter.api.DisplayNameGenerator;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class jsonUtil {
     private jsonUtil() {
         // Should not run
     }
-    public static String jsonToString(String jsonPath) throws FileNotFoundException {
+
+    public static String jsonFileToString(String jsonPath) throws FileNotFoundException {
         File file = new File(jsonPath);
         FileInputStream inputStream = new FileInputStream(file);
         return IOUtils.toString(inputStream, StandardCharsets.UTF_8);
--- uwuno/src/test/java/com/learning/uwuno/integration/tests/setUpBasicRoomTest.java
+++ uwuno/src/test/java/com/learning/uwuno/integration/tests/setUpBasicRoomTest.java
@@ -1,26 +1,81 @@
 package com.learning.uwuno.integration.tests;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.learning.uwuno.integration.jsonUtil;
+import com.learning.uwuno.util.parser;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static com.learning.uwuno.integration.constants.*;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
 
 public class setUpBasicRoomTest {
-    @Test
-    public void createValidPlayerAndRoom() throws FileNotFoundException {
-        String roomFile = jsonUtil.jsonToString(JSON_REQUESTS_PATH + "/postRoomNoBlank.json");
-        String playerFile = jsonUtil.jsonToString(JSON_REQUESTS_PATH + "/postPlayer.json");
+
+    private static String roomId;
+    private static String playerId;
+
+    @BeforeAll
+    public static void createValidRoom() throws IOException {
+        String filePath = JSON_REQUESTS_PATH + "/postRoomNoBlank.json";
+        String request = jsonUtil.jsonFileToString(filePath);
+        String requestName = new parser(request).getValue("roomName");
 
         Response response = given().contentType(ContentType.JSON)
-                .when().body(roomFile).post(BASE_URL + "/rooms")
+                .when().body(request).post(BASE_URL + "/rooms")
                 .then().extract().response();
 
-        System.out.println("request: " + roomFile);
-        System.out.println("response: " + response.asString());
+        System.out.println("Request: " + request);
+        System.out.println("Response: " + response.getBody().asString());
+
+        assertThat(response.statusCode(), is(equalTo(200)));
+        assertThat(response.path("uid"), is(not(emptyString())));
+        assertThat(response.path("name"), is(equalTo(requestName)));
+        assertThat(response.path("players"), is(empty()));
+
+        roomId = response.path("uid");
+        System.out.println("first fn: " + roomId);
     }
+
+    @Test
+    public void createValidFirstPlayer() throws IOException {
+        String filePath = JSON_REQUESTS_PATH + "/postPlayer.json";
+        String request = jsonUtil.jsonFileToString(filePath);
+        String requestName = new parser(request).getValue("name");
+
+        System.out.println("second fn: " + roomId);
+
+        Response response = given().contentType(ContentType.JSON)
+                .when().body(request).post(BASE_URL + "/rooms" + "/" + roomId + "/players")
+                .then().extract().response();
+
+        System.out.println("Request: " + request);
+        System.out.println("Response: " + response.asString());
+
+        assertThat(response.statusCode(), is(equalTo(200)));
+        assertThat(response.path("pid"), is(not(emptyString())));
+        assertThat(response.path("name"), is(equalTo(requestName)));
+        assertThat(response.path("cardList"), is(empty()));
+
+        playerId = response.path("pid");
+    }
+
+    @Test
+    public void anotherTest() {
+        System.out.println("3rd func: " + playerId);
+        Response response = given().contentType(ContentType.JSON)
+                .when().get(BASE_URL + "/rooms" + "/" + roomId)
+                .then().extract().response();
+
+        assertThat(response.statusCode(), is(equalTo(200)));
+
+        System.out.println("3rd func: " + response.getBody().asString());
+    }
+
+    // TODO: write AfterEach for clean up
 }
