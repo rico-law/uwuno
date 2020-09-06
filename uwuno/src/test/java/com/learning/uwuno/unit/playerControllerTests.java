6a301cccfdc7e465abc455968ca557c8e46bda7d
Rico <rico.law@outlook.com>
2020-09-05 22:41:16 -0700

    Temp changes.

--- uwuno/src/test/java/com/learning/uwuno/unit/playerControllerTests.java
+++ uwuno/src/test/java/com/learning/uwuno/unit/playerControllerTests.java
@@ -4,6 +4,7 @@ import com.learning.uwuno.controllers.roomController;
 import com.learning.uwuno.controllers.playerController;
 
 
+import com.learning.uwuno.room;
 import com.learning.uwuno.services.gameService;
 import org.junit.jupiter.api.Test;
 
@@ -12,8 +13,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.awt.*;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
@@ -21,17 +25,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(roomController.class)
 @AutoConfigureMockMvc
-public class roomControllerTests {
+public class playerControllerTests {
     @Autowired
     private MockMvc mock;
 
     @MockBean
     private gameService service;
 
+    room room = new room("test", false);
+
 
     @Test
-    public void testRoom_POST() throws Exception {
-//        this.mock.perform(post("/Rooms")).andDo(print()).andExpect(status().isOk());
+    public void testPlayer_POST() throws Exception {
+        mock.perform(post("/Rooms")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{ \"roomName\": \"test\", \"useBlankCards\": \"false\" }"))
+                .andDo(print());
     }
 
 }
