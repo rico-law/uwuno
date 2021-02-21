a276ceaf27bc6f0a495f4ca05cfa58329afff017
Angela Chang <ayhchang11@gmail.com>
2021-02-21 11:54:42 -0800

    test card effects apply in gameState

--- uwuno/src/test/java/com/learning/uwuno/unit/gameStatePlayCardTests.java
+++ uwuno/src/test/java/com/learning/uwuno/unit/gameStatePlayCardTests.java
@@ -20,26 +20,33 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
 @ExtendWith(MockitoExtension.class)
 public class gameStatePlayCardTests {
     @Mock private player mockPlayer;
+    @Mock gameResponse mockResponse;
 
     private gameState gameState;
     private final playerList playerList = new playerList("123");
+    private room spyRoom;
+    private player spyPlayer;
+    private gameSettings spySettings;
 
     @BeforeEach
     public void setUp() {
         playerList.add(mockPlayer);
         gameState = new gameState(playerList);
+
+        spyRoom = spy(new room("room123", false, "111"));
+        populateRoomWithPlayers(spyRoom);
+        spyPlayer = spy(spyRoom.getPlayers().cur());
+        spySettings = spy(spyRoom.getGameSettings());
     }
 
     /**
      * Tests gameState.playCard().
      * No error occurs when non-wild card matches the colour of the current card in play.
      * TODO:
-     *  - Colour matches//
      *  - Check card removed from hand (1 less card, other cards not removed)
      *  - Check card replaces card in play
      */
@@ -146,49 +153,67 @@ public class gameStatePlayCardTests {
      * Tests skip turn card effect applied.
      */
     @Test
-    public void testSkipCardEffectApplies(@Mock room mockRoom, @Mock gameResponse mockResponse) {
+    public void testSkipCardEffectApplies() {
         card toPlay = new sColorCard(card.CardType.Skip, card.Color.Yellow);
         card lastPlayed = new basicCard(2, card.Color.Yellow);
 
-        populatePlayerList();
-        player player = playerList.next();
-        player spyPlayer = spy(player);
-        gameSettings spySettings = spy(new gameSettings());
-//        room spyRoom = spy(new room("room", false, "111"));
-
         doReturn(true).when(spyPlayer).playCard(toPlay);
         doReturn(new ArrayList<>(Arrays.asList(toPlay, lastPlayed))).when(spyPlayer).getCardList();
+        doReturn(spySettings).when(spyRoom).getGameSettings();
         doReturn(30).when(spySettings).getMaxTurn();
-        when(mockRoom.getGameSettings()).thenReturn(spySettings);
-        when(mockRoom.nextPlayer(true)).thenReturn(playerList.next());
-        //Works when called from test function
-//        mockRoom.nextPlayer(true);
-//        System.out.println(playerList.cur().getName());
-//        doReturn(spySettings).when(spyRoom).getGameSettings();
-//        doReturn(playerList.next()).when(spyRoom).nextPlayer(true);
 
         boolean result = gameState.playCard(toPlay, lastPlayed, spyPlayer);
-//        gameState.endTurn(spyPlayer, spyRoom, mockResponse);
-        gameState.endTurn(spyPlayer, mockRoom, mockResponse);
         assertThat(result, is(true));
-        verify(mockRoom, times(2)).nextPlayer(true);
-        assertThat(playerList.cur().getName(), is("player3"));
 
-        // Mock keeping track of playerList pointer. Figure out how to make it keep track.
-
+        gameState.endTurn(spyPlayer, spyRoom, mockResponse);
+        verify(spyRoom, times(2)).nextPlayer(true);
+        assertThat(spyRoom.getPlayerTurn().getName(), is("player3"));
     }
 
     /**
      * Tests reverse card effect applied.
      */
+    @Test
+    public void testReverseCardEffectApplies() {
+        card toPlay = new sColorCard(card.CardType.Reverse, card.Color.Yellow);
+        card lastPlayed = new basicCard(2, card.Color.Yellow);
+
+        doReturn(true).when(spyPlayer).playCard(toPlay);
+        doReturn(new ArrayList<>(Arrays.asList(toPlay, lastPlayed))).when(spyPlayer).getCardList();
+        doReturn(spySettings).when(spyRoom).getGameSettings();
+        doReturn(30).when(spySettings).getMaxTurn();
+
+        boolean result = gameState.playCard(toPlay, lastPlayed, spyPlayer);
+        assertThat(result, is(true));
+
+        gameState.endTurn(spyPlayer, spyRoom, mockResponse);
+        verify(spyRoom, times(1)).nextPlayer(false);
+        assertThat(spyRoom.getPlayerTurn().getName(), is("player4"));
+    }
 
     /**
      * Tests player draws penalty cards from draw2 and draw4 effects.
      */
+    @Test
+    public void testDraw2Draw4CardEffectApply() {
+        card firstToPlay = new sColorCard(card.CardType.Draw2, card.Color.Yellow);
+        card secondToPlay = new wildCard(card.CardType.Draw4);
+        card lastPlayed = new basicCard(2, card.Color.Yellow);
+        doReturn(true).when(spyPlayer).playCard(firstToPlay);
+        doReturn(true).when(spyPlayer).playCard(secondToPlay);
+        gameState.playCard(firstToPlay, lastPlayed, spyPlayer);
+        gameState.playCard(secondToPlay, lastPlayed, spyPlayer);
 
-    private void populatePlayerList() {
-        for (int i = 1; i < 4; i++ ) {
-            playerList.add(new player("player" + i));
+        doReturn(new ArrayList<>(Arrays.asList(lastPlayed, lastPlayed))).when(spyPlayer).drawCards(6);
+        doReturn(lastPlayed).when(spyRoom).getLastPlayedCard();
+
+        gameState.skipTurn(spyPlayer, spyRoom, mockResponse);
+        verify(spyPlayer, times(1)).drawCards(6);
+    }
+
+    private void populateRoomWithPlayers(room room) {
+        for (int i = 1; i < 5; i++ ) {
+            room.addPlayer(new player("player" + i));
         }
     }
 }
